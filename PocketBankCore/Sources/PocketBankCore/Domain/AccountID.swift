612bf43a3eb95705baafabbf9c811da17c96fc12
karmic <karbemichael@gmail.com>
2026-08-07 22:01:50 +0200

    fix(core): update filename -> AccountID; feat(core): add constructos

--- PocketBankCore/Sources/PocketBankCore/Domain/AccountID.swift
+++ PocketBankCore/Sources/PocketBankCore/Domain/AccountID.swift
@@ -9,4 +9,12 @@ import Foundation
 
 public struct AccountID: Sendable, Hashable, Codable {
   public let value: UUID
+
+  public init() {
+    self.value = UUID()
+  }
+
+  public init(value: UUID) {
+    self.value = value
+  }
 }
